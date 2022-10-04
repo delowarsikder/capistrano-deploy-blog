7a748078a64bf15ce31277fe13836bf908471e01
delowarsikder <delowarsikder099@gmail.com>
2022-10-04 19:03:21 +0600

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-
-  namespace 'api' do
-    namespace 'v1' do
-      resources :articles 
-    end
-  end
-  # get 'articles/index' 
+  get 'comments/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
-
   resources :articles do
     resources :comments
   end
   # Defines the root path route ("/")
-  # root "articles#index"
 end
