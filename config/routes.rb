9e8150ea2a4569ebfbf31f70a00ee581ac2669d6
delowarsikder <delowarsikder099@gmail.com>
2022-10-04 19:03:21 +0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'comments/create'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+  resources :articles do
+    resources :comments
+  end
+  # Defines the root path route ("/")
+end
