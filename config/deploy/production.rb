f4bb661b1b29acff43257a8dcc13bb5bc6c6c087
delowarsikder <delowarsikder099@gmail.com>
2023-04-14 10:59:04 +0600

    change server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '3.92.20.142', user: 'ubuntu', roles: %w[web app db]
+server '54.225.17.43', user: 'ubuntu', roles: %w[web app db]
 set :ssh_options, {
   forward_agent: true,
   auth_methods: %w[publickey],
